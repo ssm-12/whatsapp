7686e5d1e68d9e7acbb432eaa7c1e4dc4a8ab1c6
Souvik Roy <souvikroy.it@gmail.com>
2018-06-10 15:16:53 +0530

    Final Commit

--- js/myCustomJs.js
+++ js/myCustomJs.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
     $("#btnUnread").click(function(){
+    	if($(window).width()>768)
+    	{
+    		$("#rightPanel").removeClass("d-none");
+    	}
         $("#unread1, #unread2, #unread3, #unread4").removeClass("d-none");
         $("#leftPanel").removeClass("d-none");
         $("#leftProfilePanel").addClass("d-none");
     });
 
     $("#btnMainPage").click(function(){
+    	if($(window).width()>768)
+    	{
+    		$("#rightPanel").removeClass("d-none");
+
+    		$("#rightPanel").removeClass("col-md-12 col-lg-12");
+    		$("#rightPanel").addClass("col-md-7 col-lg-8 d-none d-md-block d-lg-block");	
+    		$("#leftPanel").removeClass("d-none");
+    		$("#chatMessages").addClass("d-none");
+    	}
         $("#unread1, #unread2, #unread3, #unread4").addClass("d-none");
         $("#chatMessages").addClass("d-none");
         $("#leftPanel").removeClass("d-none");
@@ -29,6 +42,10 @@ $(document).ready(function() {
     });
 
     $("#btnProfilePage").click(function(){
+    	if($(window).width()<768)
+    	{
+    		$("#rightPanel").addClass("d-none");
+    	}
         $("#leftPanel").addClass("d-none");
         $("#leftProfilePanel").removeClass("d-none");
     });
@@ -39,16 +56,21 @@ $(document).ready(function() {
     });
 
     $(".openChat").click(function(){
+    	// $("#leftProfilePanel").addClass("d-none");
     	if($(window).width()<768)
     	{
 	    	$("#leftPanel").addClass("d-none");
+	    	$("#leftProfilePanel").addClass("d-none");
 	    	$("#chatMessages").removeClass("d-none");
 	    	$("#rightPanel").removeClass("col-md-7 col-lg-8 d-none d-md-block d-lg-block");
 	    	$("#rightPanel").addClass("col-md-12 col-lg-12");
     	}
     	else
     	{
+    		$("#rightPanel").removeClass("col-md-12 col-lg-12");
+    		$("#rightPanel").addClass("col-md-7 col-lg-8 d-none d-md-block d-lg-block");
     		$("#leftPanel").removeClass("d-none");
+    		$("#leftProfilePanel").addClass("d-none");
 	        $("#chatMessages").removeClass("d-none");
 	        $("#leftProfilePanel").addClass("d-none");
     	}
