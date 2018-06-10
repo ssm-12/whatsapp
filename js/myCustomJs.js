e1f5889ea9de66b0cbb9a92db509a8cdb6790222
Souvik Roy <souvikroy.it@gmail.com>
2018-06-10 14:41:54 +0530

    Everything is working now

--- js/myCustomJs.js
+++ js/myCustomJs.js
@@ -13,17 +13,61 @@ $(document).ready(function() {
     });
 
     $("#btnChatPage").click(function(){
-        $("#chatMessages").removeClass("d-none");
-        $("#leftProfilePanel").addClass("d-none");
+    	if($(window).width()<768)
+    	{
+	    	$("#leftPanel").addClass("d-none");
+	    	$("#chatMessages").removeClass("d-none");
+	    	$("#rightPanel").removeClass("col-md-7 col-lg-8 d-none d-md-block d-lg-block");
+	    	$("#rightPanel").addClass("col-md-12 col-lg-12");
+    	}
+    	else
+    	{
+    		$("#leftPanel").removeClass("d-none");
+	        $("#chatMessages").removeClass("d-none");
+	        $("#leftProfilePanel").addClass("d-none");
+    	}
     });
 
     $("#btnProfilePage").click(function(){
-        $("#leftPanel").toggleClass("d-none");
-        $("#leftProfilePanel").toggleClass("d-none");
+        $("#leftPanel").addClass("d-none");
+        $("#leftProfilePanel").removeClass("d-none");
     });
 
     $("#goBack").click(function(){
     	$("#leftPanel").removeClass("d-none");
         $("#leftProfilePanel").addClass("d-none");
+    });
+
+    $(".openChat").click(function(){
+    	if($(window).width()<768)
+    	{
+	    	$("#leftPanel").addClass("d-none");
+	    	$("#chatMessages").removeClass("d-none");
+	    	$("#rightPanel").removeClass("col-md-7 col-lg-8 d-none d-md-block d-lg-block");
+	    	$("#rightPanel").addClass("col-md-12 col-lg-12");
+    	}
+    	else
+    	{
+    		$("#leftPanel").removeClass("d-none");
+	        $("#chatMessages").removeClass("d-none");
+	        $("#leftProfilePanel").addClass("d-none");
+    	}
+    });
+
+    $("#closeChatWindow").click(function(){
+    	if($(window).width()<768)
+    	{
+	    	$("#leftPanel").removeClass("d-none");
+	    	$("#chatMessages").addClass("d-none");
+	    	$("#rightPanel").removeClass("col-md-12 col-lg-12");
+	    	$("#rightPanel").addClass("col-md-7 col-lg-8 d-none d-md-block d-lg-block");
+    	}
+    	else
+    	{
+    		$("#rightPanel").removeClass("col-md-12 col-lg-12");
+    		$("#rightPanel").addClass("col-md-7 col-lg-8 d-none d-md-block d-lg-block");	
+    		$("#leftPanel").removeClass("d-none");
+    		$("#chatMessages").addClass("d-none");
+    	}
     });
 });
